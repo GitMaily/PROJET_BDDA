872297f43245044665266609f97580872ac03800
WazupGuy <tanguy.thomas78500@gmail.com>
2022-10-20 12:27:38 +0200

    record

--- PROJET_BDDA_LAVALLEE_TANGUY_CIAVALDINI/CODE/IDE_PROJET_LAVALLEE_TANGUY_CIAVALDINI/src/Record.java
+++ PROJET_BDDA_LAVALLEE_TANGUY_CIAVALDINI/CODE/IDE_PROJET_LAVALLEE_TANGUY_CIAVALDINI/src/Record.java
@@ -1,8 +1,10 @@
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 public class Record {
 
 	private RelationInfo relInfo; // Relation à laquelle appartient le record
-	private String[] values;
+	private ArrayList<String> values;
 	
 	public Record(RelationInfo relInfo) {
 		this.relInfo=relInfo;
@@ -16,14 +18,62 @@ public class Record {
 	public void setRelInfo(RelationInfo relInfo) {
 		this.relInfo = relInfo;
 	}
-	public String[] getValues() {
+	public ArrayList<String> getValues() {
 		return values;
 	}
-	public void setValues(String[] values) {
+	public void setValues(ArrayList<String> values) {
 		this.values = values;
 	}
 	
+	public void writeToBuffer(ByteBuffer buff, int pos) {
+		
+		String type;
+		
+		buff.position(pos);
+		for(int i =0; i< values.size();i++) {
+			type = relInfo.getType_col()[i];
+			
+			switch(type) {
+			case "INTEGER" : int vInt = Integer.valueOf(values.get(i));
+			buff.putInt(vInt);
+			break;
+			
+			case "REAL" : float vFloat = Float.valueOf(values.get(i));
+			buff.putFloat(vFloat);
+			break;
+			
+			default : int vString = Integer.valueOf(values.get(i).substring(8).replace(")", ""));
+			buff.putInt(vString);
+			}
+		}
+	}
 	
+
+	void readFromBuffer(ByteBuffer buff, int pos) {
+		buff.position(pos);
+		String rel;
+		
+		for(int i=0; i<relInfo.getNb_col();i++) {
+			rel= relInfo.getType_col()[i];
+			switch (rel) {
+			case "INTEGER" : values.add(i,Integer.toString(buff.position()));
+						buff.position(buff.position()+Integer.BYTES);		
+			break;
+			
+			case "REAL" :  values.add(i,Float.toString(buff.getFloat(buff.position())));
+						buff.position(buff.position()+Float.BYTES);
+			break;
+			
+			default : int tmp = Integer.parseInt(rel.substring(8).replace(")", ""));
+				StringBuffer sb = new StringBuffer();
+				for(int j = 0; j<tmp; j++) {
+					sb.append(buff.getChar(j));
+				}
+				values.add(sb.toString());
+			}
+		}
+	}
+
 	
 	
 }
