5187807f0c67337523bd9b9258e453e2e60478a4
CIAVALDINI MAILY <il01193@migre-1.ens.math-info.univ-paris5.fr>
2022-11-24 15:39:21 +0100

    FileManager fini. Vérifier avec des tests éventuellement

---
+++ PROJET_BDDA_LAVALLEE_TANGUY_CIAVALDINI/CODE/IDE_PROJET_LAVALLEE_TANGUY_CIAVALDINI/src/RecordIterator.java
@@ -0,0 +1,40 @@
+import java.nio.ByteBuffer;
+
+public class RecordIterator {
+	private RelationInfo relInfo;
+	private PageId pageId;
+	private static int posCourante = -1;
+	private ByteBuffer bbIterator;
+	
+	public RecordIterator(RelationInfo relInfo, PageId pageId) {
+		ByteBuffer bbIterator = BufferManager.getInstance().GetPage(pageId);
+		
+		bbIterator.rewind();
+		
+		
+	}
+	
+	public Record getNextRecord() {
+		int m = bbIterator.getInt(DBParams.pageSize - Integer.BYTES * 2);
+		if(posCourante <= m) {
+			Record rec = new Record(relInfo);
+			rec.readFromBuffer(bbIterator, posCourante);
+			posCourante++;
+			return rec;
+		}
+		else {
+			return null;
+		}
+	}
+	
+	public void close() {
+		BufferManager.getInstance().FreePage(pageId, true);
+		//posCourante = -1;
+	}
+	
+	public void reset() {
+		posCourante = -1;
+	}
+	
+
+}
