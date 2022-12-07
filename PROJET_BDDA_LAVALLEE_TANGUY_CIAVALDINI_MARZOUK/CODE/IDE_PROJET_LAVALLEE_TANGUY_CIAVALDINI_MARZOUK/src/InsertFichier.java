35ff27cf6d4b2bb4da9f120f23e65a7f4a2f9ab4
WazupGuy <91200330+WazupGuy@users.noreply.github.com>
2022-12-07 15:52:22 +0100

    insertfichier finis

--- PROJET_BDDA_LAVALLEE_TANGUY_CIAVALDINI_MARZOUK/CODE/IDE_PROJET_LAVALLEE_TANGUY_CIAVALDINI_MARZOUK/src/InsertFichier.java
+++ PROJET_BDDA_LAVALLEE_TANGUY_CIAVALDINI_MARZOUK/CODE/IDE_PROJET_LAVALLEE_TANGUY_CIAVALDINI_MARZOUK/src/InsertFichier.java
@@ -11,22 +11,64 @@ public class InsertFichier {
 		this.ValRecord= new ArrayList<>();
 	}
 	
+	
+	
+	private void remplirValRecord() {
+		Readcsv l_c;
+		try{
+			l_c = new lireCsv(NomFichier);
+			ValRecord = new ArrayList<>();
+			try{
+				ValRecord= l_c.lireCsv();
+			}catch(IOException e) {
+                e.printStackTrace();
+            }
+		}catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+		
+	}
+	
+	private void remplirCmd(){
+		for(int i =0; i>ValRecord.size();i++){
+			String entree = "INSERT INTO" + this.nomRel +"VALUES (";
+			String sasir = chaine + ValRecord.get(i);
+			try{
+				InsertCommande ic = new InsertCommande(sasir);
+				ic.execute();
+			}catch (IOException e) {
+  	          e.printStackTrace(); 
+			}
+		}
+	}
+		
+
+
+	public void insererFichier() {
+
+		remplirValRecord();
+		remplirCmd();
+
+		
+	}
+
+
 	public void catchinfos(String cmd) {
 		String[] cmdSepare = cmd.split(" ");
 		this.nomRel = cmdSepare[2];
 		String[] fichierSepare = cmdSepare[3].split( "\\" );
 		String nomFichier = fichierSepare[1].substring(0,fichierSepare[1].length()-1);
 		this.NomFichier= nomFichier;
+	
 	}
-	
-	private void remplirValRecord() {
-		Readcsv l_c;
-		
+
+	public void affiche(){
+		for(int i=0; i<ValRecord.size(); i++){
+			System.out.println(ValRecord.get(i));
+		}
 	}
-	
-	public void insererFichier() {
-		
+
+	public String toString(){
+		return this.nomRel+" "+ this.nomFichier;
 	}
-	
-	
 }
