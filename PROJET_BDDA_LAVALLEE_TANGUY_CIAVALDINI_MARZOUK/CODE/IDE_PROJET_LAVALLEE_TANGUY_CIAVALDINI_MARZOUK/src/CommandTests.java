6bf3ea7ea6cdddfdb6fc7aa55ae85f140544b772
WazupGuy <91200330+WazupGuy@users.noreply.github.com>
2022-12-08 13:34:11 +0100

    sdfghjkl

--- PROJET_BDDA_LAVALLEE_TANGUY_CIAVALDINI_MARZOUK/CODE/IDE_PROJET_LAVALLEE_TANGUY_CIAVALDINI_MARZOUK/src/CommandTests.java
+++ PROJET_BDDA_LAVALLEE_TANGUY_CIAVALDINI_MARZOUK/CODE/IDE_PROJET_LAVALLEE_TANGUY_CIAVALDINI_MARZOUK/src/CommandTests.java
@@ -2,11 +2,12 @@
 public class CommandTests {
 	public static void main(String []args) {
 		//insertCommandTest();
+		//dropDBTest();
 		
 		selectCommandTest();
 		//createTableTest();
 		
-		//dropDBTest();
+		
 	}
 	
 	private static void dropDBTest() {
@@ -17,13 +18,17 @@ public class CommandTests {
 	private static void createTableTest() {
 		String commande = "CREATE TABLE Profs (X:INTEGER,C2:VARCHAR(5),BLA:VARCHAR(10))";
 		String commande2 = "CREATE TABLE Profs (NOM:VARCHAR(10),UE:VARCHAR(5))";
+		String commande3 = "CREATE TABLE ToutesLesNotes (Cours:VARCHAR(10),Note:INTEGER)";
+
 		//CreateTableCommand tc = new CreateTableCommand("CREATE TABLE Profs (X:INTEGER,C2:REAL,BLA:VARCHAR(10))");
-		CreateTableCommand tc1 = new CreateTableCommand(commande2);
+		//CreateTableCommand tc1 = new CreateTableCommand(commande2);
+		CreateTableCommand tc3 = new CreateTableCommand(commande3);
 
 		
 		//tc.execute();
-		tc1.execute();
-		
+		//tc1.execute();
+		tc3.execute();
+
 		//System.out.println(tc.toString());
 
 		//System.out.println(tc1.toString());
@@ -33,13 +38,14 @@ public class CommandTests {
 	private static void insertCommandTest() {
 		createTableTest();
 		
-		InsertCommande ic1 = new InsertCommande("INSERT INTO Profs VALUES (Ileana,BDDA)");
+		/*InsertCommande ic1 = new InsertCommande("INSERT INTO Profs VALUES (Ileana,BDDA)");
 		InsertCommande ic2 = new InsertCommande("INSERT INTO Profs VALUES (Jean-Guy-Mailly,PROG)");
-		InsertCommande ic3 = new InsertCommande("INSERT INTO Profs VALUES (Crapez,GFE)");
+		InsertCommande ic3 = new InsertCommande("INSERT INTO Profs VALUES (Crapez,GFE)");*/
+		InsertCommande ic4 = new InsertCommande("INSERT INTO ToutesLesNotes VALUES (IF3BDDA,16)" );
 
-		System.out.println(ic1.toString());
+		System.out.println(ic4.toString());
 		
-		ic1.execute();
+		ic4.execute();
 		
 
 		
@@ -54,6 +60,7 @@ public class CommandTests {
 	
 	private static void selectCommandTest() {
 		createTableTest();
+		insertCommandTest();
 		SelectCommand sc = new SelectCommand("SELECT * FROM ToutesLesNotes WHERE Cours=IF3BDDA\n"
 				+ "AND Note>=8\n"
 				+ "AND Note<10");
